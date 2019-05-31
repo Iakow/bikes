2cbabfb023f321d4fa74f1344def5f00b609c9a1
Iakow <ponomaryk@mail.ru>
2019-05-31 19:59:43 +0300

    added docs folder

---
+++ docs/js/app.js
@@ -0,0 +1,17 @@
+new WOW({
+  mobile: true
+}).init();
+
+
+$(document).ready(function(){
+  $('a[href*=\\#]').on("click", function(e){
+    var anchor = $(this);
+
+    $('html, body').stop().animate({
+      scrollTop: $(anchor.attr('href')).offset().top
+    }, 777);
+
+    e.preventDefault();
+    return false;s
+  });
+});
